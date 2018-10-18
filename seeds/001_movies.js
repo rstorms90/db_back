8c8428f2dd510e8a7b3266b2e2937d4e6939d6be
Russ Storms <rstorms90@gmail.com>
2018-10-17 19:15:43 -0600

    added movies to database

--- seeds/001_movies.js
+++ seeds/001_movies.js
@@ -5,31 +5,73 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('movies').insert([{
             id: 1,
-            title: 'blah blah',
-            director: 'russ',
-            year: 1990,
-            rating: 0
+            title: 'WALL-E',
+            director: 'Andrew Stanton',
+            year: 2008,
+            rating: 4
           },
           {
             id: 2,
-            title: 'stinkfloyd',
-            director: 'nick',
-            year: 2015,
+            title: 'The Lord of the Rings: The Return of the King',
+            director: 'Peter Jackson',
+            year: 2003,
             rating: 5
           },
           {
             id: 3,
-            title: 'thebestmovie',
-            director: 'russ',
-            year: 2017,
-            rating: 10
+            title: 'Saving Private Ryan',
+            director: 'Steven Spielberg',
+            year: 1998,
+            rating: 5
           },
           {
             id: 4,
-            title: 'The Avengers',
-            director: 'Joss Wheden',
-            year: 2012,
-            rating: 10
+            title: 'Whiplash',
+            director: 'Damien Chazelle',
+            year: 2014,
+            rating: 5
+          },
+          {
+            id: 5,
+            title: 'Up',
+            director: 'Bob Peterson & Pete Doctor',
+            year: 2009,
+            rating: 5
+          },
+          {
+            id: 6,
+            title: 'Aladdin',
+            director: 'John Musker & Ron Clements',
+            year: 1992,
+            rating: 5
+          },
+          {
+            id: 7,
+            title: 'Raiders of the Lost Ark',
+            director: 'Steven Spielberg',
+            year: 1981,
+            rating: 4
+          },
+          {
+            id: 8,
+            title: 'Office Space',
+            director: 'Mike Judge',
+            year: 1999,
+            rating: 5
+          },
+          {
+            id: 9,
+            title: 'The Goonies',
+            director: 'Richard Donner',
+            year: 1985,
+            rating: 5
+          },
+          {
+            id: 10,
+            title: 'Amélie',
+            director: 'Jean-Pierre Jeunet',
+            year: 2001,
+            rating: 5
           }
         ])
         .then(function() {
